eed6c37b56e99c9cb287dafead5043d7a58a9755
Guy Rozendorn <guy@rzn.co.il>
2011-04-25 11:55:13 +0300

    changes in setup.py
    the most important change is that the dependencies are now defined in install_requires, and not in buildout.cfg
    the rest of the changes are minor:
    
    * added comments
    * addes more keys to the dictionary
    * moved the call to find_packages from the import stage to when we actally call setuptools.setup

--- setup.py
+++ setup.py
@@ -1,24 +1,51 @@
-from setuptools import setup as _setup
-from setuptools import find_packages
 
 SETUP_INFO = dict(
     name = 'hello',
     version = '0.1',
-    author = 'Infinidat',
-    description = 'hello world',
-    long_description = (),
-    classifiers = [],
-    install_requires = [],
+    author = 'Your Name Here',
+    author_email = 'username@infinidat.com',
+    maintainer = 'Your Name Here',
+    maintainer_email = 'username@infinidat.com',
+
+    url = 'http://www.infinidat.com',
+    keywords = 'string of space-separated keywords',
+    license = 'PSF',
+    description = 'short description',
+    long_description = ('long description'),
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: Python Software Foundation License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    # A string or list of strings specifying what other distributions need to be installed when this one is
+    # We use namespaced packages so we must require setuptools
+    install_requires = ['setuptools', 'ipython', 'goodbye'],
+
+    # A dictionary mapping names of "extras" (optional features of your project) to strings or lists of strings specifying what
+    # other distributions must be installed to support those features.
     extras_require = {},
+
+    # A string or list of strings specifying what other distributions need to be present in order for the setup script to run.
+    setup_requires = '',
 
     test_suite = '',
     tests_require = {},
 
+    dependency_links = [],
+
     namespace_packages = ['hello', ],
-    packages = find_packages('src'),
+
+    # packages = find_packages('src'),
     package_dir = {'': 'src'},
     include_package_data = True,
 
+    # A dictionary mapping entry point group names to strings or lists of strings defining the entry points
     entry_points = dict(
         console_scripts = [
             'hello = hello.script:main'],
@@ -27,8 +54,11 @@ SETUP_INFO = dict(
     )
 
 def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
     _setup(**SETUP_INFO)
+
 if __name__ == '__main__':
     setup()
-
 
