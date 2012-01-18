c0a4b5bc39a28ce99845d4379ba2cb2dc48d419e
Guy Rozendorn <guy@rzn.co.il>
2012-01-18 13:33:33 +0200

    install hooks

--- src/infi/hello/scripts/__init__.py
+++ src/infi/hello/scripts/__init__.py
@@ -2,7 +2,20 @@ from __future__ import print_function
 __import__("pkg_resources").declare_namespace(__name__)
 
 from sys import argv
+import os
+
+INSTALLDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
 
 def main(argv=argv):
     print('hello world')
+
+def _write_file(name):
+    with open(os.path.join(INSTALLDIR, name), 'w') as fd:
+        fd.write(' ')
+
+def post_install(argv=argv):
+    _write_file('post_install')
+
+def pre_uninstall(argv=argv):
+    _write_file('pre_uninstall')
 
